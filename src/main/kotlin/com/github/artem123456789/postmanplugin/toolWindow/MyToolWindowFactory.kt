b2e5cf5ba91f83abe3823726c780fe3d903be332
artem <artem@mai.ru>
2024-08-04 13:15:28 +0500

    plugin ready for publishing

--- src/main/kotlin/com/github/artem123456789/postmanplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/artem123456789/postmanplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,45 +1,25 @@
 package com.github.artem123456789.postmanplugin.toolWindow
 
-import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.JBPanel
-import com.intellij.ui.content.ContentFactory
-import com.github.artem123456789.postmanplugin.MyBundle
-import com.github.artem123456789.postmanplugin.services.MyProjectService
-import javax.swing.JButton
+import com.intellij.ui.jcef.JBCefBrowser
+import java.awt.BorderLayout
+import javax.swing.JPanel
 
 
 class MyToolWindowFactory : ToolWindowFactory {
 
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
-
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        val toolWindowPanel = JPanel(BorderLayout())
+        val browser = JBCefBrowser("https://postman.com")
+
+        toolWindowPanel.add(browser.component, BorderLayout.CENTER)
+
+        val contentFactory = toolWindow.contentManager.factory
+        val content = contentFactory.createContent(toolWindowPanel, "", false)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
-
-    class MyToolWindow(toolWindow: ToolWindow) {
-
-        private val service = toolWindow.project.service<MyProjectService>()
-
-        fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
-
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
-                }
-            })
-        }
-    }
 }
